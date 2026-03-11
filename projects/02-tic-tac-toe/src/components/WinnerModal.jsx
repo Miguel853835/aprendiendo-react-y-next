23c2697c954cac19efb61e9e8073704ddfcbfcd9
Miguel853835 <miguel.s.izurieta@gmail.com>
2026-03-11 16:26:34 +0100

    cambios

--- projects/02-tic-tac-toe/src/components/WinnerModal.jsx
+++ projects/02-tic-tac-toe/src/components/WinnerModal.jsx
@@ -1,24 +1,23 @@
-import { Square } from "./Square"
+import { Square } from './Square'
 
-export function WinnerModal({winner, resetGame}){
-    // Si no hay ganador, no devuelve nada (ni el modal ni el fondo)
-    if (winner === null) return null
-    // si winner es false, es empate, si no, se muestra el ganador
-    const winnerText = winner === false ? 'Empate' : 'El ganador es:'
-    return(
-    <section className="winner">
-    <div className="text">
-        <h2>{winnerText}</h2>
-        <header className="win">
-        {/*este && es como un IF, si winner es true, muestra el cuadro del ganador (el simbolo), si es false no muestra nada */}
-        {winner && <Square>{winner}</Square>}
+export function WinnerModal ({ winner, resetGame }) {
+  // Si no hay ganador, no devuelve nada (ni el modal ni el fondo)
+  if (winner === null) return null
+  // si winner es false, es empate, si no, se muestra el ganador
+  const winnerText = winner === false ? 'Empate' : 'El ganador es:'
+  return (
+    <section className='winner'>
+      <div className='text'>
+          <h2>{winnerText}</h2>
+          <header className='win'>
+          {/* este && es como un IF, si winner es true, muestra el cuadro del ganador (el simbolo), si es false no muestra nada */}
+          {winner && <Square>{winner}</Square>}
         </header>
-        <footer>
-        {/* botón para jugar de nuevo, hace el reset para limpiar los datos */}
-        <button onClick={resetGame}>Empezar de nuevo</button>
+          <footer>
+          {/* botón para jugar de nuevo, hace el reset para limpiar los datos */}
+          <button onClick={resetGame}>Empezar de nuevo</button>
         </footer>
-    </div>
-    </section>
-    )
+        </div>
+    </section> 
+  )
 }
-
