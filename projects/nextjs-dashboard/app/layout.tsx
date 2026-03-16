e3d303e02f85a7506dcad03e20c6cfa4ba213575
Miguel853835 <miguel.s.izurieta@gmail.com>
2026-03-16 10:21:57 +0100

    Curso de Next

--- projects/nextjs-dashboard/app/layout.tsx
+++ projects/nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,17 @@
-import "@/app/ui/global.css"
+import '@/app/ui/global.css'
 import {inter} from '@/app/ui/fonts'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    //title: 'Invoices | Acme Dashboard',
+    title: {
+        template: '%s | Acme Dashboard',
+        default: 'Acme Dahboard',
+    },
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    metadataBase: new URL('https://next-earn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
